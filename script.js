29a95d2a1604c162b4a7095c56d249290304fb11
juhatakanen <juhatakanen@hotmail.com>
2021-01-12 14:06:27 +0200

    Last moving logo

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 const logo = document.querySelector('.logoPng');
 const logoDiv = document.querySelector('.logoDiv')
+const navbar = document.querySelector('.navbar')
 
 window.addEventListener('scroll', () => {
 logoDiv.classList.add('active')
+navbar.classList.add('visible')
 })
 
 logo.addEventListener('click', () => {
